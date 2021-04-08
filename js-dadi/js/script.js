3a5cf384d6a73dd10ae15504fa5f6a1c364ab1a8
Pievisco89 <pie-89@live.it>
2021-04-08 16:57:38 +0200

    aggiunto css - esercizio dadi finito

--- js-dadi/js/script.js
+++ js-dadi/js/script.js
@@ -10,9 +10,11 @@ var msg;
 if(num_utente > num_computer){
   msg = "Congratulazioni! Hai vinto!"
 }else if(num_utente === num_computer){
-  msg = "Pareggio"
+  msg = "Pareggio!"
 }else{
-  msg = "Peccato, hai perso! Ha vinto il computer."
+  msg = "Peccato, hai perso! Ha vinto il computer!"
 }
 
 document.getElementById('output').innerHTML = msg;
+document.getElementById('punteggio_utente').innerHTML = "Numero Utente:<br> " + num_utente;
+document.getElementById('punteggio_computer').innerHTML = "Numero Computer:<br> " + num_computer;
