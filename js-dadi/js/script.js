0a03319db0682930daded69048cc91d142e14715
Pievisco89 <pie-89@live.it>
2021-04-08 16:04:30 +0200

    esercizio dadi

---
+++ js-dadi/js/script.js
@@ -0,0 +1,18 @@
+/* generare numero random da 1 a 6 utente e pc */
+var num_utente = Math.floor(Math.random() * 6) + 1;
+console.log(num_utente)
+var num_computer = Math.floor(Math.random() * 6) + 1;
+console.log(num_computer)
+
+/* punteggio più alto vince */
+var msg;
+
+if(num_utente > num_computer){
+  msg = "Congratulazioni! Hai vinto!"
+}else if(num_utente === num_computer){
+  msg = "Pareggio"
+}else{
+  msg = "Peccato, hai perso! Ha vinto il computer."
+}
+
+document.getElementById('output').innerHTML = msg;
